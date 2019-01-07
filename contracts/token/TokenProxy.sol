cde950bbdc4aee2010a01b25a46c409ee364a1a8
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-01-07 18:42:54 +0800

    Fix parameters

--- contracts/token/TokenProxy.sol
+++ contracts/token/TokenProxy.sol
@@ -3,13 +3,12 @@ pragma solidity ^0.4.24;
 import "./dataStorage/TokenStorage.sol";
 import "zos-lib/contracts/upgradeability/UpgradeabilityProxy.sol";
 import "../helpers/Ownable.sol";
-import "./dataStorage/StorageState.sol";
 
 /**
 * @title TokenProxy
 * @notice A proxy contract that serves the latest implementation of TokenProxy.
 */
-contract TokenProxy is UpgradeabilityProxy, TokenStorage, Ownable, StorageState {
+contract TokenProxy is UpgradeabilityProxy, TokenStorage, Ownable {
     constructor(address _implementation, address _balances, address _allowances, string _name, uint8 _decimals, string _symbol) 
     UpgradeabilityProxy(_implementation) 
     TokenStorage(_balances, _allowances, _name, _decimals, _symbol) public {
