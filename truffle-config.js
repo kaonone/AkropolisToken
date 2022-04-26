cb08ad623cc060e509f5899c9ca0c522dc722d64
imin314 <imin314@gmail.com>
2022-04-26 12:54:17 +0700

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,58 +1,71 @@
-
-const HDWalletProvider = require("truffle-hdwallet-provider");
-require('dotenv').config()  // Stores environment-specific variable from '.env' to process.env
+const HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config(); // Stores environment-specific variable from '.env' to process.env
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-    plugins: ["truffle-plugin-verify"],
-  	compilers: {
-      solc: {
-        version: "0.4.24"  // Change this to whatever you need
-      }
-  	},
-	networks: {
-		development: {
-			host: 'localhost',
-			port: 8545,
-			network_id: '*',
-			gas: 5000000,
-      gasPrice: 55000000000
-		},
+  plugins: ['truffle-plugin-verify'],
+  compilers: {
+    solc: {
+      version: '0.4.24', // Change this to whatever you need
+    },
+  },
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      gas: 10000000,
+      gasPrice: 55000000000,
+    },
 
-		mainnet: {
-			  provider: function () {
-	          return new HDWalletProvider(process.env.METAMASK_MNEMONIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY, 3)
-	      },
-		  network_id: 1,
-		  gasPrice: 90000000000,
-	      skipDryRun:true
-		},
-		
-		kovan: {
-	          provider: function () {
-	          return new HDWalletProvider(process.env.METAMASK_MNEMONIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY)
-	      },
-	      network_id: 42,
-	      gas: 7000000
-		},
-		
-		rinkeby: {
-			provider: function () {
-			return new HDWalletProvider(process.env.METAMASK_MNEMONIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY, 1)
-		},
-		network_id: 4,
-		gas: 7000000
-	  }
-	},
-	mocha: {
-	   reporter: 'eth-gas-reporter',
-	   reporterOptions: {
-	     gasPrice: 21
-	   }
-	},
+    mainnet: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.METAMASK_MNEMONIC,
+          'https://mainnet.infura.io/v3/' + process.env.INFURA_API_KEY,
+          0
+        );
+      },
+      network_id: 1,
+      gasPrice: 35000000000,
+      gas: 6000000,
+      skipDryRun: true,
+    },
 
-	api_keys: {
-		etherscan: process.env.ETHERSCAN_API_KEY
-	}
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.METAMASK_MNEMONIC,
+          'https://kovan.infura.io/v3/' + process.env.INFURA_API_KEY
+        );
+      },
+      network_id: 42,
+      gas: 7000000,
+    },
+
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.METAMASK_MNEMONIC,
+          'https://rinkeby.infura.io/v3/' + process.env.INFURA_API_KEY,
+          0,
+          2
+        );
+      },
+      network_id: 4,
+      gasPrice: 3000000000,
+      gas: 7000000,
+    },
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      gasPrice: 21,
+    },
+  },
+
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY,
+  },
 };
